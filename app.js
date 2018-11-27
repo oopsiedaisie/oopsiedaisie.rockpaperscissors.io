166085ccb56141008edf4d9260675e4a28ada561
oopsiedaisie <chunwah041@gmail.com>
2018-11-26 23:30:50 -0400

    Fixes & Changes & Additions
    Fixed userChoice and computerChoice bug. Changed welcome message. Added a glow feature for button press.

--- app.js
+++ app.js
@@ -22,27 +22,36 @@ function convertToWord(letter){
 }
 
 function win(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "computer".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "computer".fontsize(3).sub();
     result_p.innerHTML = `✨ ${convertToWord(userChoice)}${smallUserWord} smacks ${convertToWord(computerChoice)}${smallCompWord}. You won ya fuckin' yute!`;
+    userChoice_div.classList.add('green-glow');
+    setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
 }
 
 function lose(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "computer".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "computer".fontsize(3).sub();
     result_p.innerHTML = `💩 ${convertToWord(userChoice)}${smallUserWord} got smacked by ${convertToWord(computerChoice)}${smallCompWord}. Get fucked m8! take that L`;
+    userChoice_div.classList.add('red-glow');
+    setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
 }
 
 function draw(userChoice, computerChoice) {
     const smallUserWord = "user".fontsize(3).sub();
     const smallCompWord = "computer".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `😪  ${convertToWord(userChoice)}${smallUserWord} is as stupid as ${convertToWord(computerChoice)}${smallCompWord}. idiotttt.. you tied smh`;
+    userChoice_div.classList.add('gray-glow');
+    setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
 }
 
 function resetScore() {
@@ -50,8 +59,8 @@ function resetScore() {
     computerScore = 0;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `🔰you have reset the scoreboard. fuckin' rookie `;
 }
-resetScore();
 
 function game(userChoice){
     const computerChoice = getComputerChoice();
@@ -59,17 +68,17 @@ function game(userChoice){
         case "rs":
         case "pr":
         case "sp":
-            win();
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            lose();
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            draw()
+            draw(userChoice, computerChoice);
             break;
     }
 }
@@ -77,11 +86,8 @@ game();
 
 function main(){
     rock_div.addEventListener('click', () => game("r"));
-    
     paper_div.addEventListener('click', () => game("p"));
-    
     scissors_div.addEventListener('click', () => game("s"));
-
     reset_div.addEventListener('click', () => resetScore());
 }
 main();
